344befa31d1fd098a2def5a0cbc8be27e709c544
Tainan <nerytaynan@gmail.com>
2021-01-08 00:24:56 -0300

    creating pages and styling Header component

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/styles.css'
+import Header from '../components/Header'
+const MyApp = ({Component, pageProps}) => {
+    return (
+        <div>
+            <Header/>
+            <Component {...pageProps}/>
+        </div>
+    )
+}
+export default MyApp
